5c2b487728a2a5e6459025d75e43e7a89b7eb63c
Marcin  Kurowski <mkcin@users.noreply.github.com>
2017-09-16 17:00:08 +0200

    drobne zmiany w query_set

--- query_set.py
+++ query_set.py
@@ -12,24 +12,24 @@ QUERY_SET = {
 }
 
 def prepare_query_set():
-    query = input("what to search:\n")
+    query = input("what to search (nothing given would search most popular videos I guess):\n")
     if(query == ''):
         query = None
-        print("niepoprawny format lub nie podano")
+        # print("default\n")
     QUERY_SET['query'] = query
 
-    max_result = input("max results:\n")
+    max_result = input("how many results (default 5):\n")
     if(re.match(r'^([0-9]+)$', str(max_result))):
-        max_result = int(max_result)
+        max_result = max_result
     else:
         max_result = None
-        print("niepoprawny format lub nie podano")
+        # print("default\n")
     QUERY_SET['max_result'] = max_result
 
     video_type = 'video'
     QUERY_SET['v_type'] = video_type
 
-    v_duration = input("video duration:\n1 - any\n2 - long\n3 - medium\n4 - short\n")
+    v_duration = input("video duration (default any):\n1 - any\n2 - long\n3 - medium\n4 - short\n")
     duration = {
         '1': 'any',
         '2': 'long',
@@ -38,11 +38,11 @@ def prepare_query_set():
     if(re.match(r'^([0-9]+)$', str(v_duration)) and int(v_duration) >= 1 and int(v_duration) <= 4):
         v_duration = duration[v_duration]
     else:
-        print("niepoprawny format lub nie podano")
+        # print("default\n")
         v_duration = None
     QUERY_SET['v_duration'] = v_duration
 
-    v_order = input("sort by:\n1 - relevance\n2 - upload date\n3 - viewcount\n4 - rating\n")
+    v_order = input("sort by (default relevance):\n1 - relevance\n2 - upload date\n3 - viewcount\n4 - rating\n")
     order = {
         "1": "relevance",
         "2" : "date",
@@ -51,7 +51,7 @@ def prepare_query_set():
     if(re.match(r'^([0-9]+)$', str(v_order)) and int(v_order) >= 1 and int(v_order) <= 4):
         v_order = order[v_order]
     else:
-        print("niepoprawny format lub nie podano")
+        # print("default\n")
         v_order = None
     QUERY_SET['v_order'] = v_order
 
