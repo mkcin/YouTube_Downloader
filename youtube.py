37b0fcc32a0aa39ba8c66174c2b3c3013028baca
Marcin  Kurowski <mkcin@users.noreply.github.com>
2017-09-16 14:02:28 +0200

    initial commit

---
+++ youtube.py
@@ -0,0 +1,70 @@
+'''
+Klasa Youtube pozwala na wyszukiwanie w yt za pomoca ich api
+'''
+try:
+    from urllib.request import Request, urlopen  # Python 3
+except:
+    from urllib2 import Request, urlopen  # Python 2
+
+import requests
+import json
+import youtube_dl
+
+class YoutubeSearch:
+
+    def __init__(self, apikey):
+        self.__apiKey = apikey
+        self.__constHtml = 'https://www.googleapis.com/youtube/v3/search?part=snippet'
+        self.__finhtml = ''
+        self.__searchResults = {}
+
+    def __repr__(self):
+        return ("Youtube({})".format(self.__apiKey))
+
+    def __str__(self):
+        return ("Youtube - key = {}".format(self.__apiKey))
+
+    @staticmethod
+    def headering(html, headers):
+        for header in headers:
+            html += '&'
+            html += header + "="
+            html += headers[header]
+        return html
+
+    @staticmethod
+    def remove_empty_kwargs(**kwargs):
+        good_kwargs = {}
+        if kwargs is not None:
+            try:
+                for key, value in kwargs.iteritems():
+                    if value:
+                        good_kwargs[key] = value
+            except:
+                for key, value in kwargs.items():
+                    if value:
+                        good_kwargs[key] = value
+
+        return good_kwargs
+
+    def make_request(self, **kwargs):
+        kwargs = self.remove_empty_kwargs(**kwargs)
+        kwargs['key'] = self.__apiKey
+        self.__finhtml = self.headering(self.__constHtml, kwargs)
+        response = urlopen(self.__finhtml).read().decode("utf-8")
+        self.__searchResults = json.loads(response)
+
+
+    def get_search_results(self):
+        return self.__searchResults
+
+    def get_html(self):
+        if(self.__finhtml==''):
+            return self.__constHtml
+        return self.__finhtml
+
+
+if __name__ == '__main__':
+    yt=Youtube('') # podaj klucz api od google
+    yt.make_request(order='rating', maxResults='10') # podaj zapytania(**kwargs)
+    print (yt.getSearchResults())
